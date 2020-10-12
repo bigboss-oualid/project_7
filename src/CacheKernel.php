2b04b0130c70d13801f0c025bf2083d32bda7783
BigBoss Walid <bigboss@it-bigboss.de>
2020-10-12 19:33:41 +0200

    feat: set up cache

---
+++ src/CacheKernel.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * BileMo Project.
+ *
+ * (c) 2020.  BigBoss Walid <bigboss@it-bigboss.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CacheKernel extends HttpCache
+{
+    /*protected function invalidate(Request $request, $catch = false)
+    {
+        if ('PURGE' !== $request->getMethod()) {
+            return parent::invalidate($request, $catch);
+        }
+
+        if ('127.0.0.1' !== $request->getClientIp()) {
+            return new Response(
+                'Invalid HTTP method',
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $response = new Response();
+        if ($this->getStore()->purge($request->getUri())) {
+            $response->setStatusCode(Response::HTTP_OK, 'Purged');
+        } else {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND, 'Not found');
+        }
+
+        return $response;
+    }*/
+}
